feab28e85a8d45e9334e9d702daef3dde947cfd1
Swapnika Agrawal <swapnikaagrawal@gmail.com>
2023-08-01 19:19:38 +0530

    deployed mongodb

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -48,6 +48,6 @@ func DBinstance() *mongo.Client {
 var Client *mongo.Client = DBinstance()
 
 func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-	var collection *mongo.Collection = client.Database("cluster2").Collection(collectionName)
+	var collection *mongo.Collection = client.Database("cluster0").Collection(collectionName)
 	return collection
 }
